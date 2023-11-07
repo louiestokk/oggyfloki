5cc1bc4d5f8aab65e9fb1055a3a7c902a24e2014
Louie Stokk <louiestokk@gmail.com>
2023-11-08 00:20:21 +0100

    hero img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,17 @@
 import React from 'react'
-import heroimg from '../utils/wall2.jpg'
+import heroimg from '../utils/hero1.webp'
+import logo from '../utils/logo.png'
+import MenuIcon from '@mui/icons-material/Menu';
 const Hero = () => {
   return (
     <div className='hero-container'>
+      <div className='nav-header'>
+        <img className='logo' alt='oggyfloki logo' src={logo}/>
+ <div className='inhouse-logo'>
+<MenuIcon />
+ </div>
+      
+      </div>
       <img src={heroimg} alt='oggyfloki adventure' className='hero-mainImg'/>
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,9 @@
 import React,{lazy} from 'react'
-
-const HeaderNav = lazy(()=> import('../components/HeaderNav'))
 const Hero = lazy(()=> import('../components/Hero'))
 const About = lazy(()=> import('../components/About'))
 const Home = () => {
   return (
     <div>
-        <HeaderNav />
         <Hero />
         <About/>
     </div>
