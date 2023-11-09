b68868bca28fbf9a6e8307842881274d34723c0b
Louie Stokk <louiestokk@gmail.com>
2023-11-09 19:59:51 +0100

    updates

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,8 @@ import React from 'react'
 import heroimg from '../utils/hero1.webp'
 import logo from '../utils/ourlogo.png'
 import MenuIcon from '@mui/icons-material/Menu';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import TelegramIcon from '@mui/icons-material/Telegram';
 const Hero = () => {
   return (
     <div className='hero-container'>
@@ -13,6 +15,17 @@ const Hero = () => {
       
       </div>
       <img src={heroimg} alt='oggyfloki adventure' className='hero-mainImg'/>
+<div className='hero-content'>
+  <button className='joinbtn'>PINKSALE PRESALE</button>
+  <button className='playbtn'>PLAY OGGYFLOKI</button>
+  <div style={{display:'flex',alignItems:'center'}}>
+<TelegramIcon className='icons' style={{fontSize:'2.5rem',marginRight:'0.5rem'}}/>
+  <TwitterIcon className='icons' style={{fontSize:'2.5rem',marginLeft:'0.5rem'}}/>
+  </div>
+
+</div>
+
+
 
     </div>
   )
--- src/components/Tokenomics.js
+++ src/components/Tokenomics.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import dex from '../utils/dexview.png'
-import solid from '../utils/logo_white.png'
+import karan from '../utils/karan.png'
 import expelee from '../utils/expelee_logo_light.41a503c4.png'
 import pinksales from '../utils/pinksale-logo.png'
 const Tokenomics = () => {
@@ -10,9 +10,10 @@ const Tokenomics = () => {
       <h2 style={{fontSize:'2rem'}}>Our Partners</h2>
       <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',padding:'1rem'}}>
         <img src={dex} alt='dexview logo'/>
-        <img src={solid} alt='solid proof logo'/>
         <img src={pinksales} alt='pinksale logo'/>
         <img src={expelee} alt='expelee.com logo'/>
+        <img src={karan} alt='karan tehe wizard'/>
+     
       </div>
     </div>
   )
