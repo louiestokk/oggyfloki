ce6c4c6dfdcf7cc30bfc785b5c1a42514d501e9f
Louie Stokk <louiestokk@gmail.com>
2023-11-14 22:16:26 +0100

    new comp

---
+++ src/components/OggyProfits.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import oggimg from '../utils/oggyflokiprofits.webp'
+const OggyProfits = () => {
+  return (
+    <div className='oggyprofits'>
+        <h2>Maximizing Returns with Oggy Floki!</h2>
+        <p>Profitability is a key attraction, and that's precisely what Oggy Floki is engineered to deliver to its investors. With a dual approach to enhancing holder value, Oggy Floki rewards its investors simply for holding onto their tokens while also employing a strategic token burn strategy to bolster each token's individual worth. This innovative system allows investors to potentially see their holdings and the token's value rise in tandem, fostering a passive yet dynamic growth within the Oggy Floki ecosystem.</p>
+        <img src={oggimg} alt='crypto cash'/>
+    </div>
+  )
+}
+
+export default OggyProfits
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,17 @@ const Hero = lazy(()=> import('../components/Hero'))
 const About = lazy(()=> import('../components/About'))
 const Tokenomics = lazy(()=>import('../components/Tokenomics'))
 const RoadMap = lazy(()=> import('../components/RoadMap'))
-const Gallery = lazy(()=>import('../components/Gallery'))
-const Community = lazy(()=> import('../components/Community'))
 const HowToBuy = lazy(()=> import('../components/HowToBuy'))
 const FooterNav = lazy(()=>import('../components/FooterNav'))
 const OggyDescription = lazy(()=>import('../components/OggyDescription'))
+const OggyProfits = lazy(()=>import('../components/OggyProfits'))
 const Home = () => {
   return (
     <div>
         <Hero />
         <OggyDescription />
         <About/>
+        <OggyProfits />
         <Tokenomics />
         <HowToBuy />   
             <RoadMap />
