61a59ff1caac589655a762b9e5e0bccf3bf4dc4a
Louie Stokk <louiestokk@gmail.com>
2023-11-14 20:12:10 +0100

    fix

--- src/components/RoadMap.js
+++ src/components/RoadMap.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 const RoadMap = () => {
   const boxes = [
-    { number: '1', points: ['Inception of the project and development of the token', 'Establishing social media presence and building a community.', 'Writing and publishing Medium articles and whitepaper.','Conducting a private sale and finalizing tokenomics with it included'] },
-    { number: '2', points: ['Preparing for and launching presale', 'Establishing partnerships with influencers and other meme tokens.', 'Hiring additional developers and expanding the team.','Developing token staking.','Initiating marketing campaigns and promotional activities.','Listing on decentralized exchanges and involving the community in key decisions.'] },
-    { number: '3', points: ['Launching public sale and mobile app.', 'Integrating with charities and developing a charity platform.', 'Punkt I','Expanding token holder rewards.','Pursuing partnerships with wallets and exchanges.','Listing on centralized exchanges.','Organizing community events and exploring further development of the ecosystem.'] },
+    { number: '1', points: ['Inception of the project and development of the token.', 'Establishing social media presence and building a community.', 'Writing and publishing Medium articles and whitepaper.','Conducting a private sale and finalizing tokenomics with it included.'] },
+    { number: '2', points: ['Preparing for and launching presale.', 'Establishing partnerships with influencers and other meme tokens.', 'Hiring additional developers and expanding the team.','Developing token staking.','Initiating marketing campaigns and promotional activities.','Listing on decentralized exchanges and involving the community in key decisions.'] },
+    { number: '3', points: ['Launching public sale and mobile app.', 'Integrating with charities and developing a charity platform.','Expanding token holder rewards.','Pursuing partnerships with wallets and exchanges.','Listing on centralized exchanges.','Organizing community events and exploring further development of the ecosystem.'] },
   ];
 
   return (
